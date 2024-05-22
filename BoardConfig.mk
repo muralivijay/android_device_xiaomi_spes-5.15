623a969d4123a8543d2755885a28e131634b903d
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-05-22 17:35:13 +0530

    [TMP] spes: Use permissive build for now
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,8 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=4e00000.dwc3 \
     androidboot.selinux=permissive
 
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
+
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000
 
