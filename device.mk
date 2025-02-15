053f50a41298a262e19072ba1dde7c0d2a34fee7
daniml3 <daniel@danielml.dev>
2025-02-16 02:18:13 +0530

    spes: shim the GetPreviewImageData symbol on libpiex
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libstdc++.vendor \
+	libpiex_shim \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
